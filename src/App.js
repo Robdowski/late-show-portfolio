3ef171772b391028b7df8fd446a5d3250862d615
Robert Carter <rc3331@gmail.com>
2020-02-29 23:32:48 -0600

    added playing button for theme song onto navbar, displays on all routes not home

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Route } from 'react-router-dom'
 import Main from './components/Main'
 import Nav from './components/Nav'
 import About from './components/About'
+import Portfolio from './components/Portfolio'
 
 import mp3 from './components/lateshow.mp3'
 
@@ -37,9 +38,10 @@ function App() {
   const [playing, toggle] = useAudio()
   return (
     <div className="App">
-      <Nav />
+      <Route path='/' render={(props) => <Nav {...props} playing={playing} toggle={toggle} useAudio={useAudio} />} />
       <Route exact path='/' render={(props) => <Main {...props} playing={playing} toggle={toggle} useAudio={useAudio} />} />
       <Route exact path='/about' render={(props) => <About {...props} />} />
+      <Route exact path='/portfolio' render={(props) => <Portfolio {...props} />} />
     </div>
   );
 }

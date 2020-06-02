f52c15c3da774f60e454f3d42efca59685dc6584
Robert Carter <rc3331@gmail.com>
2020-06-01 21:07:33 -0500

    added more responsiveness to navbar and about page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { motion } from 'framer-motion'
 
@@ -18,9 +18,9 @@ const Nav = (props) => {
             <h1 className='nav-title' onClick={() => handleHome()}><span className='nav-name'>Robert </span>Carter</h1>
             {props.location.pathname !== "/" && 
             <motion.div onClick={props.toggle} initial='initial' animate='animate' variants={music} transition={{duration: .5}} className="audio-player-nav">
-                <i class="fas fa-music"></i><p>{props.playing ? "Playing! (Click To Pause)" : "Play The Theme!"}</p>
-            </motion.div>}
-            <ul>
+                <i class="fas fa-music" ></i><p>{props.playing ? "Playing! (Click To Pause)" : "Play The Theme!"}</p>
+            </motion.div>}     
+            <ul className='navigation-links'>
                 <li><NavLink className='nav-link' activeClassName='selected' exact to='/'>Home</NavLink></li>
                 <li><NavLink className='nav-link' activeClassName='selected' exact to='/about'>About</NavLink></li>
                 <li><NavLink className='nav-link' activeClassName='selected' exact to='/portfolio'>Portfolio</NavLink></li>
